2c20329d87269587cb4606fbb4f26e787656c619
York <yorkemir@gmail.com>
2024-09-06 19:33:39 -0500

    Agregando ClineteTcpImc

---
+++ ClienteTcpImc/src/YorkSeveriche/imc/cliente/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package YorkSeveriche.imc.cliente;
+
+import YorkSeveriche.imc.cliente.vistas.VentanaPrincipal;
+
+/**
+ *
+ * @author York Severiche
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+        
+    }
+    
+}
