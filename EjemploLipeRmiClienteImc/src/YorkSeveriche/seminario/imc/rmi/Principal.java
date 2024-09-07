b2c85921d241dd1c38e2eac66642680a4c017046
York <yorkemir@gmail.com>
2024-09-06 19:37:00 -0500

    Agregando EjemploLipeRmiClienteImc

---
+++ EjemploLipeRmiClienteImc/src/YorkSeveriche/seminario/imc/rmi/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package YorkSeveriche.seminario.imc.rmi;
+
+import YorkSeveriche.seminario.imc.rmi.vistas.VentanaPrincipal;
+
+/**
+ *
+ * @author York Severiche
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+    
+}
